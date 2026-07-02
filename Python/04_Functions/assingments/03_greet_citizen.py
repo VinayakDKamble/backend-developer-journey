83327534d4a037a1dacf8a0f143c59d6df2b5e36
Vinayak Kamble <dkvinayak27@gmail.com>
2026-07-02 12:50:14 +0530

    Added function with parameter values

---
+++ Python/04_Functions/assingments/03_greet_citizen.py
@@ -0,0 +1,7 @@
+def greet_citizen(name):
+    print(f"Good Morning {name}")
+
+greet_citizen("vinayak")
+greet_citizen("Suresh")
+greet_citizen("Rohan")
+greet_citizen("vishal")
---
+++ Python/04_Functions/assingments/04_citizen_details.py
@@ -0,0 +1,4 @@
+def citizen_details(name, age, city):
+    print(f"The Name of the Citizen is {name} and age is {age} and the city belongs to {city}")
+
+citizen_details("vinayak",25,"Hubli")    
